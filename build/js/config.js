d678d3a737ecd58db043ebdadff3a162ce52f409
SanchezAlex <malinovskiy.alexandr@gmail.com>
2018-10-25 23:35:41 +0300

    added config.js

---
+++ build/js/config.js
@@ -0,0 +1,90 @@
+// kbp global config
+kbp = {
+    'lang': '1',
+    'title' : {
+        '0' : 'Мiжнародний аеропорт Бориспiль',
+        '1' : 'Международный аэропорт Борисполь',
+        '2' : 'Boryspil Internation Airport'
+    },
+    'boardUI' : {
+        'headers_depart' : {
+            '0' : ['Час','Рейс','Напрямок','','Перевiзник','Статус'],
+            '1' : ['Время','Рейс','Направление','','Перевозчик','Статус'],
+            '2' : ['Time','Flight','Destination','','Carrier','Status']
+        },
+        'headers_arrive' : {
+            '0' : ['Час','Рейс','Пункт вильоту','','Перевiзник','Статус'],
+            '1' : ['Время','Рейс','Пункт вылета','','Перевозчик','Статус'],
+            '2' : ['Time','Flight','From','','Carrier','Status']
+        },
+        'inprep' : {
+            '0' : 'о',
+            '1' : 'в',
+            '2' : 'at'
+        },
+        'today_tomorrow_yesterday' : {
+            '0' : {
+                'today' : 'Сьогодні',
+                'tomorrow' : 'Завтра',
+                'yesterday' : 'Вчора',
+            },
+            '1' : {
+                'today' : 'Сегодня',
+                'tomorrow' : 'Завтра',
+                'yesterday' : 'Вчера',
+            },
+            '2' : {
+                'today' : 'Today',
+                'tomorrow' : 'Tomorrow',
+                'yesterday' : 'Yesterday',
+            },
+        },
+        'statuses' : {
+            '0' : {
+                'Arrived' : 'Прибув',
+                'Boarding' : 'Посадка',
+                'BoardingStop' : 'Посадку закінчено',
+                'Cancelled' : 'Відмінено',
+                'In flight' : 'Летить',
+                'Expected' : 'Очiкується',
+                'CheckIn' : 'Реєстрація',
+                'TakeOff' : 'Зліт',
+                'Scheduled' : 'За розкладом'
+            },
+            '1' : {
+                'Arrived' : 'Прибыл',
+                'Boarding' : 'Посадка',
+                'BoardingStop' : 'Посадка закончена',
+                'Cancelled' : 'Отменен',
+                'In flight' : 'Летит',
+                'Expected' : 'Ожидается',
+                'CheckIn' : 'Регистрация',
+                'TakeOff' : 'Взлет',
+                'Scheduled' : 'По расписанию'
+            },
+            '2' : {
+                'Arrived' : 'Arrived',
+                'Boarding' : 'Boarding',
+                'BoardingStop' : 'Boarding Completed',
+                'Cancelled' : 'Cancelled',
+                'In flight' : 'In flight',
+                'Expected' : 'Expected',
+                'CheckIn' : 'Check-in',
+                'TakeOff' : 'Take Off',
+                'Scheduled' : 'Scheduled'
+            }
+
+        },
+        'messages' : {
+            '0' : {
+                'in' : 'о'
+            },
+            '1' : {
+                'in' : 'в'
+            },
+            '2' : {
+                'in' : 'in'
+            }
+        }
+    }
+};
